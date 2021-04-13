8828374267d70a2f3756e17de4a4449887c2c016
kierensharma <kieren.sharma@gmail.com>
2021-04-13 16:07:48 +0100

    Creation of Window Generator class.

--- time_series_forecasting.py
+++ time_series_forecasting.py
@@ -4,6 +4,44 @@ import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 
+class WindowGenerator():
+  def __init__(self, input_width, label_width, shift,
+               train_df, val_df, test_df,
+               label_columns=None):
+    # Store the raw data.
+    self.train_df = train_df
+    self.val_df = val_df
+    self.test_df = test_df
+
+    # Work out the label column indices.
+    self.label_columns = label_columns
+    if label_columns is not None:
+      self.label_columns_indices = {name: i for i, name in
+                                    enumerate(label_columns)}
+    self.column_indices = {name: i for i, name in
+                           enumerate(train_df.columns)}
+
+    # Work out the window parameters.
+    self.input_width = input_width
+    self.label_width = label_width
+    self.shift = shift
+
+    self.total_window_size = input_width + shift
+
+    self.input_slice = slice(0, input_width)
+    self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+    self.label_start = self.total_window_size - self.label_width
+    self.labels_slice = slice(self.label_start, None)
+    self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+  def __repr__(self):
+    return '\n'.join([
+        f'Total window size: {self.total_window_size}',
+        f'Input indices: {self.input_indices}',
+        f'Label indices: {self.label_indices}',
+        f'Label column name(s): {self.label_columns}'])
+
 def main():
     df = pd.read_csv('noon2noon_data/noon2noon_2021-02-01.csv')
 
@@ -34,12 +72,19 @@ def main():
     train_mean = train_df.mean()
     train_std = train_df.std()
 
+    train_df = (train_df - train_mean) / train_std
+    val_df = (val_df - train_mean) / train_std
+    test_df = (test_df - train_mean) / train_std    
+
     df_std = (df - train_mean) / train_std
     df_std = df_std.melt(var_name='Column', value_name='Normalized')
-    plt.figure(figsize=(12, 6))
-    ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
-    _ = ax.set_xticklabels(df.keys(), rotation=90)
-    plt.show()
+    # plt.figure(figsize=(12, 6))
+    # ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+    # _ = ax.set_xticklabels(df.keys(), rotation=90)
+    # plt.show()
+
+    w2 = WindowGenerator(input_width=6, label_width=1, shift=1, train_df=train_df, val_df=val_df, test_df=test_df, label_columns=['Heart rate'])
+    print(w2)
 
 if __name__ == '__main__':
     main()
